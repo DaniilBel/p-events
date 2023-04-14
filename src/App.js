827a278271e281d3234baf34acf91d5ff608d0a8
Daniil <danya.belyaev@gmail.com>
2023-04-14 22:41:33 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        
+        
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+        >
+          Learn React
+        </a> */}
+        <div className='row1'>
+          <div className="nf">
+            <img src="2nf_1.jpg" alt="2nf" />
+          </div>
+          <div className="nf">
+            <img src="2nf_2.jpg"  alt="2nf" />
+          </div>
+        </div>
+        <div className='row2'>
+        <div className="nf">
+            <img src="2nf_3.jpg"  alt="2nf" />
+          </div>
+          <div className="nf">
+            <img src="2nf_4.jpg"  alt="2nf" />
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
